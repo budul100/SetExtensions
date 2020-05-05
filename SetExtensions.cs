898353abf407908ff46eda8d5fd8f716ebec9ce0
Marek Grochowski <marekgrochowski@web.de>
2020-05-05 16:16:33 +0200

    Segmented funtion added

--- SetExtensions.cs
+++ SetExtensions.cs
@@ -53,6 +53,72 @@ namespace Extensions
                 .Where(s => s.Any()).ToArray();
         }
 
+        public static IEnumerable<IEnumerable<T>> Segmented<T>(this IEnumerable<IEnumerable<T>> sets)
+        {
+            var given = sets?
+                .SelectMany(g => g)
+                .Where(g => !g.IsDefault()).ToArray();
+
+            while (given?.Any() ?? false)
+            {
+                given = given
+                    .OrderBy(b => b)
+                    .ToArray();
+
+                var result = sets.GetGroups(given)
+                    .Distinct().ToArray();
+
+                yield return result;
+
+                given = given
+                    .Except(result)
+                    .ToArray();
+            }
+        }
+
         #endregion Public Methods
+
+        #region Private Methods
+
+        private static IEnumerable<T> GetGroups<T>(this IEnumerable<IEnumerable<T>> allGroups, IEnumerable<T> items)
+        {
+            var givenGroups = allGroups
+                .Where(g => g.Contains(items.First()))
+                .ToArray();
+
+            foreach (var item in items)
+            {
+                var newGroups = allGroups
+                    .Where(g => g.Contains(item)).ToArray();
+
+                if (newGroups.Count() == givenGroups.Count())
+                {
+                    var index = 0;
+                    while (index < newGroups.Count())
+                    {
+                        if (!newGroups[index].SequenceEqual(givenGroups[index]))
+                        {
+                            break;
+                        }
+
+                        index++;
+                    }
+
+                    if (index == newGroups.Count())
+                    {
+                        yield return item;
+                    }
+                }
+            }
+        }
+
+        private static bool IsDefault<T>(this T x)
+        {
+            return EqualityComparer<T>.Default.Equals(
+                x: x,
+                y: default);
+        }
+
+        #endregion Private Methods
     }
 }
